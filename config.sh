7d6e403904e4aaef29c43fdd435ff82dc265ceca
Recolic Keghart <root@recolic.net>
2020-07-04 22:52:47 +0800

    add wrapped pacman

---
+++ config.sh
@@ -0,0 +1,10 @@
+
+# Path to pacman executable
+aurpar_config_pacman_path='/usr/bin/pacman'
+
+# Threads count. Recommended value is 1.5*CPU_CORES
+aurpar_config_threads=6
+
+# Leave it there if you don't know what this means
+aurpar_config_lock_path='/var/lib/pacman/db.lck'
+
