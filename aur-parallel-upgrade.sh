391e3b1d3fa3632796579ce6d246e3213894b4ea
Recolic Keghart <root@recolic.net>
2020-07-04 23:32:25 +0800

    working

--- aur-parallel-upgrade.sh
+++ aur-parallel-upgrade.sh
@@ -1,4 +1,32 @@
 #!/bin/bash
+# Run `yaourt -Syu --noconfirm` in parallel! (between packages)
+
+####################### prepare ##########################
+aurpar_bin_name="$0"
+function where_is_him () {
+    SOURCE="$1"
+    while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+        DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+        SOURCE="$(readlink "$SOURCE")"
+        [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+    done
+    DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+    echo -n "$DIR"
+}
+function where_am_i () {
+    _my_path=`type -p ${aurpar_bin_name}`
+    where_is_him "$_my_path"
+}
+my_path=`where_am_i`
+
+source "$my_path/config.sh"
+####################### prepare ##########################
 
 
+
+exit 0
+############ notes
+# list packages:
+echo n | yaourt -Su --aur 2>/dev/null | sed 's/\r/\n/g' | grep '^aur/'
+pakku -Syup | grep aur.archlinux.org | sed 's/^.*aur.archlinux.org.//g' | sed 's/.git$//g'
 
--- config.sh
+++ config.sh
@@ -1,10 +1,52 @@
 
-# Path to pacman executable
-aurpar_config_pacman_path='/usr/bin/pacman'
-
 # Threads count. Recommended value is 1.5*CPU_CORES
 aurpar_config_threads=6
 
+# Name of your AUR package manager. Currently support: pakku, yaourt
+aurpar_config_aur_manager='yaourt'
+# aurpar_config_aur_manager='pakku'
+
+########################################################################
+
+# Path of your AUR package manager
+aurpar_config_aur_manager_path="$aurpar_config_aur_manager"
+
+# Path to pacman executable
+aurpar_config_pacman_path='/usr/bin/pacman'
+
 # Leave it there if you don't know what this means
 aurpar_config_lock_path='/var/lib/pacman/db.lck'
+
+# Some AUR repo contains multiple packages. We install it ONLY EXACTLY once!
+# TODO: recognize & skip packages automatically. Read the `pkgname` in PKGBUILD and recognize it.
+aurpar_config_package_blacklist=(
+    # intel ICC
+    # intel-common-libs # INSTALL THIS
+    intel-openmp
+    intel-compiler-base
+    intel-fortran-compiler
+    intel-ipp
+    intel-mpi
+    intel-tbb_psxe
+    intel-vtune-profiler
+    intel-advisor
+    intel-inspector
+
+    # Jetbrains
+    # clion # INSTALL THIS
+    clion-jre
+    clion-cmake
+    clion-gdb
+    clion-lldb
+    # clion-eap # INSTALL THIS
+    clion-eap-jre
+    clion-eap-cmake
+    clion-eap-gdb
+    clion-eap-lldb
+
+    # goland
+    goland-jre
+    # goland-eap
+    goland-eap-jre
+    )
 
