f587f7984ddc3af62a16a60113a28ec8cd1db226
Bensong Liu <bensl@microsoft.com>
2020-10-08 16:49:22 +0800

    working

--- aur-parallel-upgrade.sh
+++ aur-parallel-upgrade.sh
@@ -22,7 +22,33 @@ my_path=`where_am_i`
 source "$my_path/config.sh"
 ####################### prepare ##########################
 
+function do_install () {
+    # do_install $package_name
+    # NO input, outputs to stderr. 
+    PATH="$my_path:$PATH" yaourt -S --noconfirm "$1" 1>&2
+    return $?
+}
 
+function get_package_list () {
+    # NO input, NO arg. Result outputs to stdout, seprated by '\n'
+    pacman -Sy || return $?
+    if [[ "$aurpar_config_aur_manager" == "yaourt" ]]; then
+        echo n | "$aurpar_config_aur_manager_path" -Su --aur 2>/dev/null | sed 's/\r/\n/g' | grep '^aur/'
+        return $?
+    elif [[ "$aurpar_config_aur_manager" == "pakku" ]]; then
+        "$aurpar_config_aur_manager_path" -Syup | grep aur.archlinux.org | sed 's/^.*aur.archlinux.org.//g' | sed 's/.git$//g'
+        return $?
+    else
+        echo "ERROR: Unknown package manager '$aurpar_config_aur_manager'. Supports: yaourt, pakku" 1>&2
+        return 1
+    fi
+}
+
+pkgs=`get_package_list` || { echo "Failed to get package list. Exiting..." 1>&2; exit $?; }
+
+echo "============ Packages to UPGRADE: "
+echo "$pkgs"
+echo "$pkgs" | parallel --max-procs "$aurpar_config_threads" do_install '{}' || echo "Done. But there is some failure. " 1>&2
 
 exit 0
 ############ notes
